ec566c873cb8abdf57b7819f9c642c880081ee2b
yuri-gagarin <ymivanov@icloud.com>
2022-08-04 13:49:07 +0300

    let's talk about canvas

--- components/breakout/GameScreen.tsx
+++ components/breakout/GameScreen.tsx
@@ -9,10 +9,34 @@ export interface IGameScreenProps {
 export const GameScreen: React.FunctionComponent<IGameScreenProps> = (props: IGameScreenProps): JSX.Element => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
+  const drawBricks = (canvasRef: React.MutableRefObject<HTMLCanvasElement | null>): void => {
+
+  };
+  const renderBall = (canvasRef: React.MutableRefObject<HTMLCanvasElement | null>): void => {
+    if (!canvasRef.current) return;
+    let x: number = 0;
+    const render = () => {
+      const canvas = canvasRef.current as HTMLCanvasElement;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+        ctx.arc(x, 50, 20, 0, 2 * Math.PI);
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 4;
+        ctx.fill();
+        ctx.stroke();
+        x += 8;
+        requestAnimationFrame(render);
+      } 
+    }
+    render();
+  }
+
   useEffect(() => {
     if (canvasRef.current) {
-      const ctx = canvasRef.current.getContext("2d");
-      
+      renderBall(canvasRef)
     }
   }, [ canvasRef.current ]);
   return (
