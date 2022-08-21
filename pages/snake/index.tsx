2d69f4e5b2f2788df895423e29352b06882d858a
yuri-gagarin <ymivanov@icloud.com>
2022-08-21 23:34:01 +0300

    let's do a snake

---
+++ pages/snake/index.tsx
@@ -0,0 +1,29 @@
+import React, { useContext, useReducer } from 'react';
+import { Grid } from "semantic-ui-react";
+// additional components //
+import { Board } from '../../components/snake/Board';
+import snakeReducer from '../../context/reducers/snakeReducer';
+// context ///
+import { SnakeContext } from '../../context/SnakeContext';
+
+interface ISnakeIndexProps {
+  
+}
+
+export const SnakeIndex: React.FunctionComponent<ISnakeIndexProps> = (props): JSX.Element => {
+  // context //
+  const initState = useContext(SnakeContext);
+  const [ snakeState, dispatch ] = useReducer(snakeReducer, initState);
+  return (
+    <>
+      <Grid.Row>
+        <Grid.Column>
+          <Board snakeState={ snakeState } dispatch={ dispatch } />
+        </Grid.Column>
+      </Grid.Row>
+    </>
+  )
+  ;
+};
+
+export default SnakeIndex;
