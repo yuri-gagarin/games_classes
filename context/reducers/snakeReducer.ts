726ca88813a4161f7598a108fdc829d3ff499775
yuri-gagarin <ymivanov@icloud.com>
2022-08-22 23:41:50 +0300

    snakereducer

---
+++ context/reducers/snakeReducer.ts
@@ -0,0 +1,40 @@
+import { GameState, SnakeAction } from "../actions/breakout/breakOutActions";
+
+export type SnakeState = {
+  gameState: GameState;
+  difficulty: 1 | 2 | 3 | 4;
+  score: number;
+  lives: number;
+};
+export const INIT_STATE: SnakeState = {
+  gameState: "NewGame",
+  difficulty: 1,
+  score: 0,
+  lives: 5
+};
+
+export default function SnakeReducer(state: SnakeState = INIT_STATE, action: SnakeAction): SnakeState {
+  const { type, payload } = action;
+  
+  switch (type) {
+    case "SetGameState": {
+      return {
+        ...state,
+        ...action.payload
+      };
+    }
+    case "IncrementScore": {
+      return {
+        ...state,
+        score: state.score + action.payload.score
+      };
+    }
+    case "DecrementLives": {
+      return {
+        ...state,
+        lives: state.lives = action.payload.lives
+      };
+    }
+    default: return state;
+  }
+};
