af410458d48b0b3d91c29d1e09c9875a31e053f2
yuri-gagarin <ymivanov@icloud.com>
2022-09-09 16:34:13 +0300

    board with cell values

--- components/snake/Board.tsx
+++ components/snake/Board.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { SnakeState } from '../../context/reducers/snakeReducer';
 import styles from "../../styles/snake/SnakeBoard.module.css";
 
 
@@ -25,6 +26,20 @@ class SingleLinkedList {
 
 const BOARD_SIZE: number = 10;
 
+const createBoard = (BOARD_SIZE: number): number[][] => {
+  let cellVal = 1;
+  const board: number[][] = [];
+  for (let i = 0; i < BOARD_SIZE; i++) {
+    const row: number[] = [];
+    for (let j = 0; j < BOARD_SIZE; j++) {
+      row.push(cellVal);
+      cellVal++;
+    }
+    board.push(row);
+  }
+  return board;
+}
+
 
 const drawBoard = (boardSize: number): string[][] => {
   let counter: number = 1;
@@ -33,10 +48,12 @@ const drawBoard = (boardSize: number): string[][] => {
   })
 }
 interface ISnakeBoardProps {
+  snakeState: SnakeState;
+  dispatch: React.Dispatch<any>;
 }
 
 export const SnakeBoard: React.FunctionComponent<ISnakeBoardProps> = (props) => {
-  const [ board, setBoard ] = useState<string[][]>(drawBoard(10));
+  const [ board, setBoard ] = useState<number[][]>(createBoard(BOARD_SIZE));
   const [ snakeCells, setSnakeCells ] = useState<Set<number>>(new Set([44]));
   const [ snake, setSnake ] = useState(new SingleLinkedList(44));
   return (
@@ -48,7 +65,8 @@ export const SnakeBoard: React.FunctionComponent<ISnakeBoardProps> = (props) => 
               {
                 row.map((cellVal, indexOfCell) => {
                   return (
-                    <div key={ "cell" + "_" + indexOfCell } className={ `${styles.snakeBoardCell} ${snakeCells.has(parseInt(cellVal)) && styles.snakeCell}` }>
+                    <div key={ "cell" + "_" + indexOfCell } className={ `${styles.boardCell} ${snakeCells.has(cellVal) && styles.snakeCell}` }>
+                      {cellVal}
                     </div>
                   )
                 })
--- pages/snake/index.tsx
+++ pages/snake/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useReducer } from 'react';
 import { Grid } from "semantic-ui-react";
 // additional components //
-import { Board } from '../../components/snake/Board';
+import { SnakeBoard } from '../../components/snake/Board';
 import snakeReducer from '../../context/reducers/snakeReducer';
 // context ///
 import { SnakeContext } from '../../context/SnakeContext';
@@ -18,7 +18,7 @@ export const SnakeIndex: React.FunctionComponent<ISnakeIndexProps> = (props): JS
     <>
       <Grid.Row>
         <Grid.Column>
-          <Board snakeState={ snakeState } dispatch={ dispatch } />
+          <SnakeBoard snakeState={ snakeState } dispatch={ dispatch } />
         </Grid.Column>
       </Grid.Row>
     </>
