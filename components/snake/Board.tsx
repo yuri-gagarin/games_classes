c30921b4505ffc5ac089c9c6bdebaf666d747c3d
yuri-gagarin <ymivanov@icloud.com>
2022-09-13 23:42:32 +0300

    move snake something... how about those coordinates

--- components/snake/Board.tsx
+++ components/snake/Board.tsx
@@ -1,27 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SnakeState } from '../../context/reducers/snakeReducer';
 import styles from "../../styles/snake/SnakeBoard.module.css";
 
 
 class LinkedListNode<T> {
-  private value: T;
-  private next: T| null;
+  private _value: T;
+  private _next: T| null;
   constructor(value: T) {
-    this.value = value;
-    this.next = null;
+    this._value = value;
+    this._next = null;
+  }
+  get value(): T {
+    return this._value;
+  }
+  get next(): T | null {
+    return this._next;
   }
 };
 
+
 class Cell  {
-  private row: number;
-  private column: number;
-  private nextHeadVal: number;
+  private _row: number;
+  private _column: number;
+  private _nextHeadVal: number;
+
   constructor(row: number, column: number, nextHeadVal: number) {
-    this.row = row;
-    this.column = column;
-    this.nextHeadVal = nextHeadVal;
+    this._row = row;
+    this._column = column;
+    this._nextHeadVal = nextHeadVal;
+  }
+
+  get row(): number {
+    return this._row;
+  }
+  get column(): number {
+    return this._column;
+  }
+  get nextHeadVal(): number {
+    return this._nextHeadVal;
   }
 };
+
+enum Direction {
+  UP = "UP",
+  RIGHT = "RIGHT",
+  DOWN = "DOWN",
+  LEFT = "LEFT",
+  NONE = "NONE"
+};
+const getDirectionFromKey = (key: string): Direction => {
+  if (key === "ArrowUp") return Direction.UP;
+  if (key === "ArrowRight") return Direction.RIGHT;
+  if (key === "ArrowDown") return Direction.DOWN;
+  if (key === "ArrowLeft") return Direction.LEFT;
+  return Direction.NONE;
+}
 
 class SingleLinkedList<T> {
   private _head: LinkedListNode<T>;
@@ -34,6 +67,9 @@ class SingleLinkedList<T> {
 
   get head(): LinkedListNode<T> {
     return this._head;
+  }
+  get next(): LinkedListNode<T> {
+    return this._next;
   }
 }
 
@@ -62,14 +98,37 @@ interface ISnakeBoardProps {
 
 export const SnakeBoard: React.FunctionComponent<ISnakeBoardProps> = (props) => {
   const [ board, setBoard ] = useState<number[][]>(createBoard(BOARD_SIZE));
+  const [ snake, setSnake ] = useState(new SingleLinkedList<Cell>(new Cell(6, 5, 45)));
   const [ snakeCells, setSnakeCells ] = useState<Set<number>>(new Set([44]));
-  const [ snake, setSnake ] = useState(new SingleLinkedList<Cell>(new Cell(5, 5, 44)));
   //
-  const moveSnake = () => {
-    const currentHeadCoord = { 
-      row: snake.head
+
+  const getHextHeadCoords = (currentHeadCoords: Cell, direction: Direction) => {
+    if (direction === Direction.UP) {
+
+    } else if (direction === Direction.RIGHT) {
+
+    } else if (direction === Direction.DOWN) {
+
+    } else if (direction === Direction.LEFT) {
+
+    } else {
+      return;
     }
   }
+  const moveSnake = () => {
+    const currentHeadCoord = { 
+      row: snake.head.value.row,
+      column: snake.head.value.column
+    };
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (getDirectionFromKey(e.key) === Direction.NONE) return;
+      console.log(getDirectionFromKey(e.key));
+    })
+  }, []);
+
   return (
     <div className={ styles.snakeBoard }>
       {
